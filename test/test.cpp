6d6c202f00200271da1d0995f2ae64dbea55837d
Dongyul LEE <eastheat88@tinnos.com>
2015-01-09 18:16:46 +0900

    second version

--- test/test.cpp
+++ test/test.cpp
@@ -4,21 +4,17 @@
 #include "stdafx.h"
 #include "test.h"
 
-#define MAX_LOADSTRING 100
-
 // 전역 변수:
-HWND hWndp;
 HINSTANCE hInst;								// 현재 인스턴스입니다.
-TCHAR szTitle[MAX_LOADSTRING];					// 제목 표시줄 텍스트입니다.
-TCHAR szWindowClass[MAX_LOADSTRING];			// 기본 창 클래스 이름입니다.
 
 // 이 코드 모듈에 들어 있는 함수의 정방향 선언입니다.
-ATOM				MyRegisterClass(HINSTANCE hInstance);
+
 BOOL				InitInstance(HINSTANCE, int);
-LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
-LRESULT CALLBACK	WndProcb(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
 
+
+Window win1;
+mybutton b1;
 int APIENTRY _tWinMain(HINSTANCE hInstance,
                      HINSTANCE hPrevInstance,
                      LPTSTR    lpCmdLine,
@@ -26,81 +22,112 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 {
 	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(lpCmdLine);
-	hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
 
-	// TODO: 여기에 코드를 입력합니다.
+ 	// TODO: 여기에 코드를 입력합니다.
 	MSG msg;
-
-	Window win(L"window",L"TINNOS",WS_OVERLAPPEDWINDOW,0, 0, 500, 500, NULL, NULL, hInstance, NULL);
-	win.init();
-	win.creat();
-	ShowWindow(win.GetHandle(), nCmdShow);
-	UpdateWindow(win.GetHandle());
-
-	myButton button(L"mybutton",L"button",WS_CHILD|WS_VISIBLE,50,50,100,30, win.GetHandle(), NULL, hInst, NULL);
-	button.init();
-	button.creat();
 	
+	win1.create(_T("test1"),NULL);
+	//mybutton b1;
+	b1.create(_T("mybutton"),win1.GetHWND());
+
+
 	// 기본 메시지 루프입니다.
 	while (GetMessage(&msg, NULL, 0, 0))
 	{
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
 	}
 
 	return (int) msg.wParam;
 }
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK Window::staticWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-	int wmId, wmEvent;
-	PAINTSTRUCT ps;
-	HDC hdc;
+	Window* pPwindow= (Window*)GetProp(hWnd, _T("wincreate"));
+	mybutton* pPmybutton= (mybutton*)GetProp(hWnd, _T("mybutton"));
+	
+	if(hWnd==win1.GetHWND())
+		win1.handleMessage(hWnd, message, wParam, lParam);
+	if(hWnd==b1.GetHWND())
+		b1.handleMessage(hWnd,message,wParam,lParam);
 
+	return DefWindowProc(hWnd, message, wParam, lParam);
 
+}
+
+// 정보 대화 상자의 메시지 처리기입니다.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
 	switch (message)
 	{
-	case WM_COMMAND:
-		wmId    = LOWORD(wParam);
-		wmEvent = HIWORD(wParam);
-		// 메뉴 선택을 구문 분석합니다.
-		switch (wmId)
-		{
-		case IDM_ABOUT:
-			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-			break;
-		case IDM_EXIT:
-			DestroyWindow(hWnd);
-			break;
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
 
-		case WM_LBUTTONUP:
-		//hdc=GetDC(hWnd);
-		//ReleaseDC(hWnd, hdc);
-		MessageBox(hWnd,L"First Button Clicked",L"Button",MB_OK);
-		break;
-		default:
-			return DefWindowProc(hWnd, message, wParam, lParam);
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
 		}
 		break;
-
-
-	case WM_PAINT:
-		hdc = BeginPaint(hWnd, &ps);
-		// TODO: 여기에 그리기 코드를 추가합니다.
-		EndPaint(hWnd, &ps);
-		break;
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		break;
-	default:
-		return DefWindowProc(hWnd, message, wParam, lParam);
 	}
-	return 0;
+	return (INT_PTR)FALSE;
 }
 
 
 
-LRESULT CALLBACK WndProcb(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+mybutton::mybutton()
+{
+	hWnd=NULL;
+	pWnd=NULL;
+}
+
+mybutton::~mybutton()
+{
+	RemoveProp(hWnd,_T("mybutton"));
+}
+
+bool mybutton::create(const LPCWSTR TitleName,HWND pWnd)
+{
+	WNDCLASS wc;
+	DWORD		dwStyle;
+	hInstance = GetModuleHandle(NULL);
+
+
+	wc.style		=	CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+	wc.lpfnWndProc	=	(WNDPROC) staticWndProc;
+	wc.cbClsExtra	=	0;
+	wc.cbWndExtra	=	0;
+	wc.hInstance	=	hInstance;
+	wc.hIcon		=	LoadIcon(NULL, IDI_WINLOGO);
+	wc.hCursor		=	LoadCursor(NULL,IDC_ARROW);
+	wc.hbrBackground=	(HBRUSH)GetStockObject(GRAY_BRUSH);
+	wc.lpszMenuName	=	NULL;
+	wc.lpszClassName=	_T("mybutton");
+
+	if(!RegisterClass(&wc))
+	{
+		MessageBox(NULL,_T("Failed To Register The Window Class."),_T("ERROR"),MB_OK|MB_ICONEXCLAMATION);		
+		return FALSE;
+	}
+	
+	dwStyle=WS_CHILD|WS_VISIBLE;	
+
+	hWnd=CreateWindow(_T("mybutton"),TitleName,dwStyle,50,50,100,30,pWnd,NULL,hInstance,this);
+
+	if(!hWnd)
+	{
+		MessageBox(NULL,_T("mybutton Creation Error."),_T("ERROR"),MB_OK|MB_ICONEXCLAMATION);
+		return FALSE;
+	}
+
+	SetProp(hWnd, _T("mybutton"), (HANDLE)this);
+	
+	return TRUE;
+}
+
+LRESULT mybutton::handleMessage(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	int wmId, wmEvent;
 	PAINTSTRUCT ps;
@@ -159,107 +186,98 @@ LRESULT CALLBACK WndProcb(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	return 0;
 }
 
-
-
-
-// 정보 대화 상자의 메시지 처리기입니다.
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+Window::Window()
 {
-	UNREFERENCED_PARAMETER(lParam);
-	switch (message)
-	{
-	case WM_INITDIALOG:
-		return (INT_PTR)TRUE;
+	hWnd=NULL;
+}
 
-	case WM_COMMAND:
-		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-		{
-			EndDialog(hDlg, LOWORD(wParam));
-			return (INT_PTR)TRUE;
-		}
-		break;
+Window::~Window()
+{
+	RemoveProp(hWnd,_T("wincreate"));
+}
+
+bool Window :: create(const LPCWSTR TitleName, HWND pWnd)
+{
+	WNDCLASS wc;
+	DWORD		dwStyle;
+	hInstance = GetModuleHandle(NULL);
+
+
+	wc.style		=	CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+	wc.lpfnWndProc	=	(WNDPROC) staticWndProc;
+	wc.cbClsExtra	=	0;
+	wc.cbWndExtra	=	0;
+	wc.hInstance	=	hInstance;
+	wc.hIcon		=	LoadIcon(NULL, IDI_WINLOGO);
+	wc.hCursor		=	LoadCursor(NULL,IDC_ARROW);
+	wc.hbrBackground=	NULL;
+	wc.lpszMenuName	=	MAKEINTRESOURCE(IDC_TEST);
+	wc.lpszClassName=	_T("wincreate");
+
+	if(!RegisterClass(&wc))
+	{
+		MessageBox(NULL,_T("Failed To Register The Window Class."),_T("ERROR"),MB_OK|MB_ICONEXCLAMATION);		
+		return FALSE;
 	}
-	return (INT_PTR)FALSE;
-}
-
-
-
-Window::Window(const LPCWSTR ClassNmae, const LPCWSTR TitleName,DWORD dwStyle, int x, int y, int nWidth, int nHeight, HWND hWndParent, HMENU hMenu, HINSTANCE hInstance, LPVOID lpParam)
-{
-	this->ClassNmae=ClassNmae;
-	this->TitleName=TitleName;
-	this->dwStyle=dwStyle;
-	this->x=x;
-	this->y=y;
-	this->nWidth=nWidth;
-	this->nHeight=nHeight;
-	this->hWndParent=hWndParent;
-	this->hMenu=hMenu;
-	this->hInstance=hInstance;
-	this->lpParam=lpParam;
-
-}
-
-ATOM Window::init()
-{
 	
-	wcex.cbSize = sizeof(WNDCLASSEX);
-	wcex.style			= CS_HREDRAW | CS_VREDRAW;
-	wcex.lpfnWndProc	= WndProc;
-	wcex.cbClsExtra		= 0;
-	wcex.cbWndExtra		= 0;
-	wcex.hInstance		= hInstance;
-	wcex.hIcon			= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_TEST));
-	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
-	wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
-	wcex.lpszMenuName	= MAKEINTRESOURCE(IDC_TEST);
-	wcex.lpszClassName	= ClassNmae;
-	wcex.hIconSm		= LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+	dwStyle=WS_OVERLAPPEDWINDOW;	
 
-	return RegisterClassEx(&wcex);
-}
+	hWnd=CreateWindow(_T("wincreate"),TitleName,dwStyle,0,0,500,500,NULL,NULL,hInstance,this);
 
-bool Window::creat()
-{
-	
-	hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
-	hWnd = CreateWindow(ClassNmae, TitleName, dwStyle, x, y, nWidth, nHeight, hWndParent, hMenu, hInstance, lpParam);
-	
-	if (!hWnd)
+	if(!hWnd)
 	{
+		MessageBox(NULL,_T("Window Creation Error."),_T("ERROR"),MB_OK|MB_ICONEXCLAMATION);
 		return FALSE;
 	}
 
-   return TRUE;
+//	SetProp(hWnd, _T("wincreate"), (HANDLE)this);
+	
+	ShowWindow(hWnd, SW_SHOW);
+	UpdateWindow(hWnd);
+	
+	return TRUE;
 }
 
-HWND Window:: GetHandle()
+LRESULT Window::handleMessage( HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	int wmId, wmEvent;
+	PAINTSTRUCT ps;
+	HDC hdc;
+	
+	
+	switch (message)
+	{
+	case WM_COMMAND:
+		wmId    = LOWORD(wParam);
+		wmEvent = HIWORD(wParam);
+		// 메뉴 선택을 구문 분석합니다.
+		switch (wmId)
+		{
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+		break;
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		// TODO: 여기에 그리기 코드를 추가합니다.
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+HWND Window::GetHWND()
 {
 	return hWnd;
-}
-
-myButton::myButton(const LPCWSTR ClassNmae, const LPCWSTR TitleName,DWORD dwStyle, int x, int y, int nWidth, int nHeight, HWND hWndParent, HMENU hMenu, HINSTANCE hInstance, LPVOID lpParam)
-	:Window(ClassNmae,TitleName,dwStyle,x,y,nWidth,nHeight,hWndParent,hMenu,hInstance,lpParam)
-{
-
-}
-
-ATOM myButton::init()
-{
-	
-	wcex.cbSize = sizeof(WNDCLASSEX);
-	wcex.style			= CS_HREDRAW | CS_VREDRAW;
-	wcex.lpfnWndProc	= WndProcb;
-	wcex.cbClsExtra		= 0;
-	wcex.cbWndExtra		= 0;
-	wcex.hInstance		= hInstance;
-	wcex.hIcon			= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_TEST));
-	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
-	wcex.hbrBackground	= (HBRUSH)GetStockObject(WHITE_BRUSH);
-	wcex.lpszMenuName	= MAKEINTRESOURCE(IDC_TEST);
-	wcex.lpszClassName	= ClassNmae;
-	wcex.hIconSm		= LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
-
-
-	return RegisterClassEx(&wcex);
 }
--- test/test.h
+++ test/test.h
@@ -5,32 +5,26 @@
 
 class Window
 {
-public :
-	Window(const LPCWSTR ClassNmae, const LPCWSTR TitleName,DWORD dwStyle, int x, int y, int nWidth, int nHeight, HWND hWndParent, HMENU hMenu, HINSTANCE hInstance, LPVOID lpParam);
-	
-	virtual ATOM init();
-	bool creat();
-	void handleMessage();
-	HWND GetHandle();
+public:
+	static LRESULT CALLBACK staticWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+	Window();
+	~Window();
+	virtual bool create(const LPCWSTR TitleName,HWND pWnd);
+	virtual LRESULT handleMessage(HWND, UINT, WPARAM, LPARAM);
+	HWND GetHWND();
 
 protected:
-	WNDCLASSEX wcex;
-	LPCWSTR ClassNmae;
-	LPCWSTR TitleName;
-	DWORD dwStyle;
-	int x, y, nWidth, nHeight;
-	HWND hWndParent;
-	HMENU hMenu;
-	HINSTANCE hInstance;
-	LPVOID lpParam;
+	HWND pWnd;
 	HWND hWnd;
+	HINSTANCE hInstance;
 };
 
 
-class myButton : public Window
+class mybutton : public Window
 {
 public:
-	myButton(const LPCWSTR ClassNmae, const LPCWSTR TitleName,DWORD dwStyle, int x, int y, int nWidth, int nHeight, HWND hWndParent, HMENU hMenu, HINSTANCE hInstance, LPVOID lpParam);
-	ATOM init();
+	mybutton();
+	~mybutton();
+	virtual LRESULT handleMessage(HWND, UINT, WPARAM, LPARAM);
+	virtual bool create(const LPCWSTR TitleName,HWND pWnd);
 };
-
